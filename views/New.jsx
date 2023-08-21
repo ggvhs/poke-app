19972c81df81c0f8a12b8e188d20c7e418042ea2
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-08-21 14:36:14 -0400

    form in my new route

---
+++ views/New.jsx
@@ -0,0 +1,15 @@
+function New() {
+    return (
+      <div>
+          <h1>New Pokemon Page</h1>
+  
+          <form action="/pokemon" method='POST'>
+              Name:<input type="text" name='name'/> <br />
+              url: <input type="url" name='url'/> <br />
+              <input type="submit" value="create Pokemon" />
+          </form>
+      </div>
+    )
+  }
+  
+  module.exports = New
