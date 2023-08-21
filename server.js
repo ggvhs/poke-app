c02006c67e1ce6e853dcdf7a535a24fbe71d0eeb
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-08-20 21:43:14 -0400

    all packages are added

---
+++ server.js
@@ -0,0 +1,45 @@
+// const express = require("express");
+// const pokemon = require("./models/pokemon.js")
+
+
+// const app = express();
+// const port = 3007;
+
+// // SETTING UP VIEW ENGINE
+// app.set("views", `${__dirname}/views`);
+// app.set('view engine', 'jsx');
+// app.engine('jsx', require('express-react-views').createEngine());
+
+
+// //LISTENER
+// app.listen(port , (req, res) => {
+//     console.log(`port is listening on ${port}`)
+// })
+
+// //ROUTE
+// app.get('/', (req,res) => {
+//     res.send(`<h1>Welcome to the pokemon app...</h1>`)
+// })
+
+// app.get('/pokemon', (req,res) => {
+//     // res.send(pokemon)
+//     res.render("Index", {
+//         pokemon: pokemon
+//     })
+// })
+
+
+
+/*
+EXAMPLES
+
+app.get("/fruits", (req, res) => {
+    res.render("fruits/Index", {
+        fruits: fruits
+    });
+});
+
+
+DIV FOR LATER
+
+*/
---
+++ views/Index.jsx
@@ -0,0 +1,36 @@
+// import React from 'react';
+
+
+// module.exports = function Index({ pokemon }) {
+
+//     const myStyle = {
+//         color: '#ffffff',
+//         backgroundColor: '#000000',
+//         width: "100vw",
+//         height: "100vh",
+//         textAlign: "center"
+//         };
+        
+       
+//     return (
+//         <div style={myStyle}>
+//             <h1>
+//                 See All Those Pokemon :3
+//             </h1>
+//         </div>
+//     )
+        
+
+
+// }
+
+
+/*
+MAKE A DIV
+
+ return (
+          <div style={myStyle}>My First React Component!</div>;
+        }
+
+*/
+
